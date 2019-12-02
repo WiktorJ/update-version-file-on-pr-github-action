2b5c3e4e7a4e0664b50b4a9ad48a41470400f2f2
w.jurasz <w.jurasz@kigroup.de>
2019-12-02 12:30:39 +0100

    deteted swp

--- index.js
+++ index.js
@@ -4,21 +4,15 @@ const semver = require('semver');
 
 
 async function run() {
-	const token = core.getInput('repo-token', {required: true});oconst client = new github.GitHub(token);
+	const token = core.getInput('repo-token', {required: true})
 	const client = new github.GitHub(token);
-
-	try {
-	  // `who-to-greet` input defined in action metadata file
-	  const nameToGreet = core.getInput('who-to-greet');
-	  console.log(`Hello ${nameToGreet}!`);
-	  const time = (new Date()).toTimeString();
-	  core.setOutput("time", time);
-	  // Get the JSON webhook payload for the event that triggered the workflow
-	  const payload = JSON.stringify(github.context.payload, undefined, 2)
-	  console.log(`The event payload: ${payload}`);
-	} catch (error) {
-	  core.setFailed(error.message);
-	}
+	client.repos.getContents({
+	'WiktorJ',
+	'tagging-test',
+	'VERSION'
+	}).then(result => {
+		console.log(result)
+	})
 }
 
 run()
