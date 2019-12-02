0f256c89d0190b1104fee6abf15fd6e62ff017fe
w.jurasz <w.jurasz@kigroup.de>
2019-12-02 12:22:30 +0100

    basic action

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const semver = require('semver');
+
+
+async function run() {
+	const token = core.getInput('repo-token', {required: true});oconst client = new github.GitHub(token);
+	const client = new github.GitHub(token);
+
+	try {
+	  // `who-to-greet` input defined in action metadata file
+	  const nameToGreet = core.getInput('who-to-greet');
+	  console.log(`Hello ${nameToGreet}!`);
+	  const time = (new Date()).toTimeString();
+	  core.setOutput("time", time);
+	  // Get the JSON webhook payload for the event that triggered the workflow
+	  const payload = JSON.stringify(github.context.payload, undefined, 2)
+	  console.log(`The event payload: ${payload}`);
+	} catch (error) {
+	  core.setFailed(error.message);
+	}
+}
+
+run()
