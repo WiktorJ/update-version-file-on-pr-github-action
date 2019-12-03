844039b4cc3c104a3582b9f8bb406e30c54e495d
w.jurasz <w.jurasz@kigroup.de>
2019-12-03 13:22:28 +0100

    removed async function

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run() {
 
 }
 
-async function readFile(path) {
+function readFile(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, { encoding: "utf8" }, (err, data) => {
       if (err) {
