4b8a82a7c6d46770c3d8eb0777e2152cb7e4419a
w.jurasz <w.jurasz@kigroup.de>
2019-12-03 13:15:37 +0100

    added version update

--- index.js
+++ index.js
@@ -18,21 +18,34 @@ async function run() {
 		console.log(version)
 		const newVersion = semver.inc(version, 'minor')
 		console.log(newVersion)
+
+		const GITHUB_EVENT_PATH = process.env.GITHUB_EVENT_PATH;
+		if (!GITHUB_EVENT_PATH) {
+		throw new Error("Environment variable GITHUB_EVENT_PATH not set!");
+		}
+
+		const eventDataStr = await readFile(GITHUB_EVENT_PATH);
+		const eventData = JSON.parse(eventDataStr);
+		//console.log(eventData);
+
+
+		const labels = eventData.pull_request.labels
+		for(const label of labels) {
+			console.log(label)
+		}
+		client.repos.createOrUpdateFile({
+			owner:  'WiktorJ',
+			repo:   'tagging-test',
+			path:   'VERSION',
+			message: 'Version updated to: ' + newVersion,
+			content: Buffer.to(newVersion, 'base64'),
+			sha: sha,
+			branch: eventData.head.ref,
+			committer.name = 'version_update_action',
+			committer.email = 'some@email.com'
+
+		})
 	})
-	const GITHUB_EVENT_PATH = process.env.GITHUB_EVENT_PATH;
-	if (!GITHUB_EVENT_PATH) {
- 	throw new Error("Environment variable GITHUB_EVENT_PATH not set!");
-	}
-
-	const eventDataStr = await readFile(GITHUB_EVENT_PATH);
-	const eventData = JSON.parse(eventDataStr);
-	//console.log(eventData);
-
-
-	const labels = eventData.pull_request.labels
-	for(const label of labels) {
-		console.log(label)
-	}
 
 }
 
