2b6668c4da9e05d3c1315f67aed0f60a9bc3afd8
w.jurasz <w.jurasz@kigroup.de>
2019-12-03 14:29:53 +0100

    switched to await

--- index.js
+++ index.js
@@ -8,50 +8,50 @@ async function run() {
 	
 	const token = core.getInput('token', {required: true})
 	const client = new github.GitHub(token);
-	client.repos.getContents({
+	const result = await client.repos.getContents({
 		owner:	'WiktorJ',
 		repo:	'tagging-test',
 		path:	'VERSION'
-	}).then(result => {
-		const sha = result.data.sha
-		const version = Buffer.from(result.data.content, 'base64').toString()
-		console.log(version)
-		const newVersion = semver.inc(version, 'minor')
-		console.log(newVersion)
-
-		const GITHUB_EVENT_PATH = process.env.GITHUB_EVENT_PATH;
-		if (!GITHUB_EVENT_PATH) {
-		throw new Error("Environment variable GITHUB_EVENT_PATH not set!");
-		}
-
-		const eventDataStr = readFile(GITHUB_EVENT_PATH);
-		const eventData = JSON.parse(eventDataStr);
-		//console.log(eventData);
-
-
-		const labels = eventData.pull_request.labels
-		for(const label of labels) {
-			console.log(label)
-		}
-		client.repos.createOrUpdateFile({
-			owner:  'WiktorJ',
-			repo:   'tagging-test',
-			path:   'VERSION',
-			message: 'Version updated to: ' + newVersion,
-			content: Buffer.to(newVersion, 'base64'),
-			sha: sha,
-			branch: eventData.head.ref,
-			committer: {
-				name: 'version_update_action',
-				email: 'some@email.com'
-			}
-
-		})
 	})
+
+	const sha = result.data.sha
+	const version = Buffer.from(result.data.content, 'base64').toString()
+	console.log(version)
+	const newVersion = semver.inc(version, 'minor')
+	console.log(newVersion)
+
+	const GITHUB_EVENT_PATH = process.env.GITHUB_EVENT_PATH;
+	if (!GITHUB_EVENT_PATH) {
+	throw new Error("Environment variable GITHUB_EVENT_PATH not set!");
+	}
+
+	const eventDataStr = await readFile(GITHUB_EVENT_PATH);
+	const eventData = JSON.parse(eventDataStr);
+	//console.log(eventData);
+
+
+	const labels = eventData.pull_request.labels
+	for(const label of labels) {
+		console.log(label)
+	}
+	client.repos.createOrUpdateFile({
+		owner:  'WiktorJ',
+		repo:   'tagging-test',
+		path:   'VERSION',
+		message: 'Version updated to: ' + newVersion,
+		content: Buffer.to(newVersion, 'base64'),
+		sha: sha,
+		branch: eventData.head.ref,
+		committer: {
+			name: 'version_update_action',
+			email: 'some@email.com'
+		}
+	})
+
 
 }
 
-function readFile(path) {
+async function readFile(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, { encoding: "utf8" }, (err, data) => {
       if (err) {
